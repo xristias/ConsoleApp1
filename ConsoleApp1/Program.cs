9ac79f86c74ac7b8fd6e6b0071718390d24d313b
xristias <panosc@windowslive.com>
2018-10-07 11:38:43 +0300

    exclamation

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //master comments
+            //master comments!
             Console.WriteLine("Hello World!");
         }
     }
